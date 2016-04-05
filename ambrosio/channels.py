894e7d7832069cadec68be5b082d2f91d9480fed
Carles Mateu <carlesm@carlesm.com>
2016-04-05 17:03:53 +0200

    Added stuff

---
+++ ambrosio/channels.py
@@ -0,0 +1,25 @@
+
+
+class Channel(object):
+    """Channel class"""
+    def __init__(self, name):
+        super(Channel, self).__init__()
+        self.name = name
+
+
+class TextChannel(object):
+    """Channel class, reads commands from file"""
+    def __init__(self, name="TextChannel"):
+        super(TextChannel, self).__init__()
+        self.name = name
+        self.messages = []
+        with open("messages.txt", "r") as f:
+            for line in f:
+                self.messages.append(line)
+
+    def get_msg(self):
+        if self.msg_avail():
+            return self.messages.pop(0)
+
+    def msg_avail(self):
+        return len(self.messages) > 0
