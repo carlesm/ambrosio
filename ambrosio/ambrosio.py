91054bd356ddf3542de41b810151361a8450e6d3
Carles Mateu <carlesm@carlesm.com>
2016-04-05 15:54:51 +0200

    Added mainloop

--- ambrosio/ambrosio.py
+++ ambrosio/ambrosio.py
@@ -2,5 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
+class Ambrosio(object):
+    """Class for Ambrosio Personal Digital Butler
+
+    Will run our house"""
+    def __init__(self):
+        super(Ambrosio, self).__init__()
+
+
+    def mainloop(self):
+        # While True:
+        #   command = get_command
+        #   do_command(command)
+        #   update
+        pass
+
 if __name__ == "__main__":
     print "Here be dragons!"
